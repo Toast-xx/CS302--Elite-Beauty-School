ca727591e7efbe4cc893ccd8ca2e1b1844f68700
MitchellWillis05 <mitchellwillis33@gmail.com>
2025-10-10 23:00:36 +1300

    updated product model

--- app/models/product.py
+++ app/models/product.py
@@ -1,6 +1,8 @@
 from app import db
 
 class Product(db.Model):
+    __tablename__ = "products"
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     price = db.Column(db.Float, nullable=False)
@@ -9,3 +11,18 @@ class Product(db.Model):
     category = db.Column(db.String(50), nullable=True)
     image_file = db.Column(db.String(255), nullable=True)  # e.g., 'product1.jpg'
     image_gallery = db.Column(db.JSON, nullable=True)      # e.g., ['img1.jpg', 'img2.jpg']
+
+    def __repr__(self):
+        return f"<Product {self.name}>"
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "price": self.price,
+            "brand": self.brand,
+            "description": self.description,
+            "category": self.category,
+            "image_file": self.image_file,
+            "image_gallery": self.image_gallery,
+        }
